e9d138f9b4d0bf76f78e0423c763508269aea773
YoshikazuHosono <yyhhwork@gmail.com>
2022-03-26 14:34:20 +0900

    tidy up

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,11 +62,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
+  target: "static",
+
   router: {
     base: "/YoshikazuHosono.github.io/",
-  },
-
-  generate: {
-    dir: "docs",
   },
 };
