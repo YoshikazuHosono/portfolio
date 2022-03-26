cc15af5e5656b0e8fb0e8ea0e3b36e261c17420e
YoshikazuHosono <yyhhwork@gmail.com>
2022-03-26 15:12:16 +0900

    tidy up

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,51 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row dense>
+      <v-col cols="12">
+        <v-card color="#FFFFFF">
+          <v-card-title class="text-h3"> Hosono Yoshikazu </v-card-title>
+
+          <v-card-subtitle class="text-h5">
+            細野の自己紹介サイト
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card color="#FFFFFF">
+          <v-card-title class="text-h3"> どんな人？ </v-card-title>
+
+          <v-card-subtitle class="text-h5"> 名前とか </v-card-subtitle>
+          <v-card-subtitle class="text-h5"> 趣味とか </v-card-subtitle>
+          <v-card-subtitle class="text-h5">
+            Twitterとかのリンク
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card color="#FFFFFF">
+          <v-card-title class="text-h3"> Skills </v-card-title>
+
+          <v-card-subtitle class="text-h5">
+            やったことある言語とか
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card color="#FFFFFF">
+          <v-card-title class="text-h3"> 職歴 </v-card-title>
+
+          <v-card-subtitle class="text-h5"> やった案件とか </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
